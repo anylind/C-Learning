924f8a41e22328976c387528796c71d8aa8abea0
anylind <noanyo@proton.me>
2026-07-09 11:41:36 +0330

    Added new project 'my-cat'.

---
+++ file-io/my-cat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+
+    unsigned char c;
+    if (argc < 2){
+        
+        while ((c = getchar()) != EOF){
+            putchar(c);
+        }
+        return 0;
+    }
+
+    FILE *file = fopen(argv[1], "rb");
+
+    while (fread(&c, sizeof c, 1, file) == 1)
+    {
+        putchar(c);
+    }
+
+    return 0;
+}
